ab17c8fbb80a4c683a322e4451d866257354119d
Linus Vettiger <linus4123@gmail.com>
2019-09-17 16:29:07 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <span>Hello, World!</span>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
