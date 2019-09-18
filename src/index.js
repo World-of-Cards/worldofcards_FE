31e4ef81dedc7334e1538cf5022163746ee0b84f
Vettiger, Linus <linus.vettiger@novartis.com>
2019-09-18 17:28:54 +0200

    removed old app definition

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import App from "./app";
 import './style/global.scss';
 
-const App = () => {
-  return <span>Hello, World!</span>;
-};
-
 ReactDOM.render(<App />, document.getElementById('root'));
