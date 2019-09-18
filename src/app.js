919cd586162edca60511faf4b1f01bd99f4f1b26
Vettiger, Linus <linus.vettiger@novartis.com>
2019-09-18 14:47:24 +0200

    Added app routing with react-router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import RouteContent from "./routes";
 
 const App = () => {
-  return <span>Hello, World!</span>;
+  return (
+    <Router>
+      <header>
+        <Link to="/search">Search</Link>
+      </header>
+      <article>
+        <RouteContent/>
+      </article>
+      <footer>Foot</footer>
+    </Router>
+  );
 };
 
 export default hot(module)(App);
